f18f3ad00b6995f9bd95df6665df73d8b7d1c459
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-14 14:06:27 +0200

    Init server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,45 @@
-console.log("work");
+import * as dotenv from "dotenv";
+import Fastify from "fastify";
+import prismaPlugin from "./plugins/prisma";
+import fastifyCors from "@fastify/cors";
+import multer from "fastify-multer";
+import fastifyHelmet from "@fastify/helmet";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const fastify = Fastify({ logger: true });
+await fastify.register(fastifyCors);
+
+fastify.register(fastifyHelmet, {
+  crossOriginResourcePolicy: { policy: "cross-origin" },
+});
+fastify.register(prismaPlugin);
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "public/assets"),
+});
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+(async () => {
+  try {
+    await fastify.listen({ port: +(process.env?.PORT ?? 3000) });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+})();
