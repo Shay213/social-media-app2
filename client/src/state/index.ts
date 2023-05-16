6432ed0a3fd69c7c73ca1ab78ab745762fb740a4
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-16 10:15:31 +0200

    Setup redux states

---
+++ client/src/state/index.ts
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface InitialState {
+  mode: string;
+  user: object | null;
+  token: string | null;
+  posts: object[];
+}
+
+const initialState = {
+  mode: 'light',
+  user: null,
+  token: null,
+  posts: [],
+} as InitialState;
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === 'light' ? 'dark' : 'light';
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error('User friends non-existatent');
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post.id === action.payload.postId) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/social-media-app2/",
+  base: '/social-media-app2/',
   plugins: [react()],
   server: {
     port: 5137,
